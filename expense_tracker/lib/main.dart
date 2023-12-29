b05091458d55f01c3e2b0a881ee821a7e0be0927
Debanjan Saha <debanjansaha2001@gmail.com>
2023-12-29 16:29:07 +0530

    integrate sms reader and material themes

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -2,30 +2,36 @@ import 'package:flutter/material.dart';
 import 'homepage.dart' as homepage;
 
 void main() {
+  // WidgetsFlutterBinding.ensureInitialized(); // add this before runApp
   runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Expense Tracker App',
       theme: ThemeData(
-        brightness: Brightness.light,
-        /* light theme settings */
+        fontFamily: 'OpenSans',
+        brightness: Brightness.light, /* light theme settings */
+         textTheme: TextTheme(
+          bodyLarge: TextStyle(color: Colors.black),
+        )
       ),
       darkTheme: ThemeData(
-        brightness: Brightness.dark,
-        /* dark theme settings */
+        fontFamily: 'OpenSans',
+        brightness: Brightness.dark,  /* dark theme settings */
+        textTheme: TextTheme(
+          bodyLarge: TextStyle(color: Colors.white),
+        )
       ),
       themeMode: ThemeMode.system, 
       /* ThemeMode.system to follow system theme, 
          ThemeMode.light for light theme, 
          ThemeMode.dark for dark theme
       */
-      debugShowCheckedModeBanner: false,
+      // debugShowCheckedModeBanner: false,
       home: homepage.Homepage(),
     );
   }
--- expense_tracker/lib/navbar.dart
+++ expense_tracker/lib/navbar.dart
@@ -3,19 +3,32 @@ import 'package:flutter/material.dart';
 class Navbar extends StatelessWidget {
   const Navbar({super.key});
 
+
   @override
   Widget build(BuildContext context) {
+    bool isDarkMode = (MediaQuery.of(context).platformBrightness == Brightness.dark);
+
     return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height * 0.075,
       padding: const EdgeInsets.all(10.0),
-      decoration: const BoxDecoration(
-        color: Colors.deepPurple,
+      decoration: BoxDecoration(
+        // borderRadius: BorderRadius.circular(10),
+        // border: Border.all(
+          // color: Colors.redAccent,
+          // width: 2, 
+        // ),
+        color: (isDarkMode ? Colors.blue: Colors.orange.shade600),
       ),
-      child: const Row(
-        children: [
-          Text('NavItem1'),
-          SizedBox(width: 50),
-          Text('NavItem2'),
-        ],
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          children: [
+            Text('NavItem1'),
+            SizedBox(width: 50),
+            Text('NavItem2'),
+          ],
+        ),
       ),
     );
   }
